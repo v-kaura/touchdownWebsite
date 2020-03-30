63e0c420e72f74bfa2324dc6b8a9a4c4981e691c
Vihaan Kaura <26315381+v-kaura@users.noreply.github.com>
2020-03-30 22:30:45 +1100

    Added sticky behaviour to the footer
    The footer will stick to the bottom of the webpage if the webpage is too short, otherwise it will be sit at the bottom of the content.

--- home.js
+++ home.js
@@ -1,12 +1,30 @@
 window.onscroll = function() {scrollFunc()};
+window.onload = function() {loadFunc()};
+
+const stickyTopClass = "stickyTop";
+const stickyBottomClass = "stickyBottom";
 
 var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+var navbarTop = navbar.offsetTop;
+
+var footer = document.getElementById("footerID");
+var footerTop = footer.offsetTop;
+var footerHeight = footer.offsetHeight;
+var footerBottom = footerTop + footerHeight;
+
+function loadFunc() {
+
+    if ((window.pageYOffset + window.innerHeight) >= footerBottom) {
+        footer.classList.add(stickyBottomClass);
+    } else {
+        footer.classList.remove(stickyBottomClass);
+    }
+}
 
 function scrollFunc() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= navbarTop) {
+        navbar.classList.add(stickyTopClass)
+    } else {
+        navbar.classList.remove(stickyTopClass);
+    }
 }
